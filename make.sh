f1f59b362badd7543c4d193c22a8a6b7ffc2c655
Dan Fitch <dgfitch@gmail.com>
2020-12-16 22:14:34 -0600

    Tweaks to get final pdfs right

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-OIFS="$IFS"
-IFS=$'\n'
-for item in `find markdown -type f -name '*.markdown'`
-do
-  echo pandoc \'$item\' -f markdown -t latex -o \'${item//markdown/tex}\';
-done
 
-IFS="$OIFS"
+lualatex book.tex
+lualatex inside.tex
+#lualatex chapbook-cover-inside.tex
+
+# Now we need to chop the first page off of book.pdf
+
+pdftk "book.pdf" cat 2-end output "book-final.pdf"
